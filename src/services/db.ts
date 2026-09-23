f9461c041d4ffa1b48f569f0035cb23efad6832d
justryan-n2d <ryan.oledan0@gmail.com>
2026-09-24 02:09:15 +0800

    feat: make Prisma database target explicit

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { getDatabaseUrl } from "../config/database";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: getDatabaseUrl()
+    }
+  }
+});
